44c84c1f8c37e25d4f1be244da20074944f25ff2
asowes12546 <36076110+asowes12546@users.noreply.github.com>
2019-11-08 19:57:59 +0800

    作業四

--- UnityProject/Assets/Boy.cs
+++ UnityProject/Assets/Boy.cs
@@ -6,6 +6,11 @@ public class Boy : MonoBehaviour
     public float speed = 1.5f;
     [Header("跳躍高度"), Range(100, 1500)]
     public int jump = 100;
+    public Animator ator;
+    public AudioSource audio;
+    public AudioClip a;
+    private bool isTouch;
+
 
     private void Update()
     {
@@ -15,5 +20,22 @@ public class Boy : MonoBehaviour
     private void Move()
     {
         transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    public void Jump()
+    {
+        if(isTouch == true) 
+        {
+            transform.position = new Vector3(0, 5, 0);
+            ator.SetBool("跳躍開關", true);
+            audio.PlayOneShot(a);
+        }
+        isTouch = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ator.SetBool("跳躍開關", false);
+        isTouch = true;
     }
 }
